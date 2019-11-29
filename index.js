ae2b96c25e138484c1c5b8d73d9cdb4a576e33de
Mother <home@mothers-mbp.lan>
2019-11-30 00:44:04 +0100

    initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 }, () => {
+  console.log("Server started");
+});
+
+wss.on("connection", connection => {
+  connection.on("message", message => {
+    // Transmettre un message à tous les autres utilisateurs connectés (broadcast)
+    wss.clients.forEach(client => {
+      if (client !== connection && client.readyState === WebSocket.OPEN) {
+        //   if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
