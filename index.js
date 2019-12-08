168ae5c13960a8c2614136d61498d5dda7cfc71c
brocolis-developpeur <mallerep@gmail.com>
2019-12-08 20:05:40 +0100

    dimanche soir

--- index.js
+++ index.js
@@ -11,12 +11,35 @@ app.get("/", (req, res) => {
 });
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server: server });
+// to give IDs to new connections
+let id_s = 0;
 
+//players list
 const playersList = {};
 
+// send playerslist only to connections with a username
+const sendPLayersList = () => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN && client.username) {
+      client.send(
+        JSON.stringify({
+          object: "newUser",
+          playersList: Object.keys(playersList)
+        })
+      );
+    }
+  });
+};
+
 wss.on("connection", connection => {
+  connection.on("close", () => {
+    //delete player from playerslist and send updated list to everybody
+    console.log("closed ", connection.username);
+    delete playersList[connection.username];
+    sendPLayersList();
+  });
+
   connection.on("message", message => {
-    // parse message
     message = JSON.parse(message);
     if (message.object) {
       console.log(message);
@@ -25,17 +48,12 @@ wss.on("connection", connection => {
     }
     switch (message.object) {
       case "enterArena":
+        //Completer la userlist
         playersList[message.username] = connection;
-        wss.clients.forEach(client => {
-          if (client.readyState === WebSocket.OPEN) {
-            client.send(
-              JSON.stringify({
-                object: "newUser",
-                playersList: Object.keys(playersList)
-              })
-            );
-          }
-        });
+        //Aussi ajouter à la connection
+        connection.username = message.username;
+        //Update playerslist to all players
+        sendPLayersList();
 
         break;
       case "challenge":
@@ -46,16 +64,23 @@ wss.on("connection", connection => {
         break;
 
       default:
-        // wss.clients.forEach(client => {
-        //   if (client !== connection && client.readyState === WebSocket.OPEN) {
-        //     client.send(JSON.stringify(message));
-        //   }
-        // });
         playersList[message.opponent].send(JSON.stringify(message));
         break;
     }
   });
 });
+
+// //Update de la liste des joueurs si certains ont quitté
+// const interval = setInterval(function ping() {
+//   wss.clients.forEach(function each(ws) {
+//     console.log(Object.keys(playersList));
+//     // Faire un nouvel Objet
+//     // Si sa longueur est differente de Playerslist
+//     // Remplacer playerslist
+//     // Remvoyer playersList a tout le monde
+//     // Cote FRONT. FILTRER le tableau avec ceux qui sont en train de jouer
+//   });
+// }, 3000);
 
 server.listen(process.env.PORT || 8080, () => {
   console.log("Server started");
