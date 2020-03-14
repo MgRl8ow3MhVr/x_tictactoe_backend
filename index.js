f96c27ae48e64f1f0196a4f3d3c93c8c8d037e82
MgRl8ow3MhVr <7anUDQ2A3MVJkC7J@gmail.com>
2020-03-14 22:46:28 +0100

    ajout deal d'un ping depuis front

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const http = require("http");
-const WebSocket = require("ws");
-const cors = require("cors");
+const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
+const cors = require('cors')
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Hello World" });
-});
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server: server });
+app.get('/', (req, res) => {
+  res.json({ msg: 'Hello World' })
+})
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server: server })
 // to give IDs to new connections
-let id_s = 0;
+let id_s = 0
 
 //players list
-const playersList = {};
+const playersList = {}
 
 // send playerslist only to connections with a username
 const sendPLayersList = () => {
@@ -23,65 +23,70 @@ const sendPLayersList = () => {
     if (client.readyState === WebSocket.OPEN && client.username) {
       client.send(
         JSON.stringify({
-          object: "newUser",
+          object: 'newUser',
           playersList: Object.keys(playersList)
         })
-      );
+      )
     }
-  });
-};
+  })
+}
 
-wss.on("connection", connection => {
-  connection.on("close", () => {
+wss.on('connection', connection => {
+  connection.on('close', () => {
     //delete player from playerslist and send updated list to everybody
-    console.log("closed ", connection.username);
-    delete playersList[connection.username];
-    sendPLayersList();
-  });
+    console.log('closed ', connection.username)
+    delete playersList[connection.username]
+    sendPLayersList()
+  })
 
-  connection.on("message", message => {
-    message = JSON.parse(message);
+  connection.on('message', message => {
+    message = JSON.parse(message)
     if (message.object) {
-      console.log(message);
+      console.log(message)
     } else {
-      console.log(message.username);
+      console.log(message.username, 'played')
     }
     switch (message.object) {
-      case "enterArena":
+      case 'enterArena':
         //Completer la userlist
-        playersList[message.username] = connection;
+        playersList[message.username] = connection
         //Aussi ajouter à la connection
-        connection.username = message.username;
+        connection.username = message.username
         //Update playerslist to all players
-        sendPLayersList();
-
-        break;
-      case "challenge":
-        console.log(message.challenger, message.opponent);
+        sendPLayersList()
+        break
+      case 'challenge':
+        console.log(message.challenger, message.opponent)
         playersList[message.opponent].send(
-          JSON.stringify({ object: "challenged", by: message.challenger })
-        );
-        break;
+          JSON.stringify({ object: 'challenged', by: message.challenger })
+        )
+        break
+
+      case 'ping':
+        console.log('just a ping from', message.player)
+        break
 
       default:
-        playersList[message.opponent].send(JSON.stringify(message));
-        break;
+        playersList[message.opponent].send(JSON.stringify(message))
+        break
     }
-  });
-});
+  })
+})
 
-// //Update de la liste des joueurs si certains ont quitté
-// const interval = setInterval(function ping() {
-//   wss.clients.forEach(function each(ws) {
-//     console.log(Object.keys(playersList));
-//     // Faire un nouvel Objet
-//     // Si sa longueur est differente de Playerslist
-//     // Remplacer playerslist
-//     // Remvoyer playersList a tout le monde
-//     // Cote FRONT. FILTRER le tableau avec ceux qui sont en train de jouer
-//   });
-// }, 3000);
+// //Ping régulier sur les joueurs pour garder le serveur eveillé
+// setInterval(function ping () {
+//   console.log('send ping to all')
+//   wss.clients.forEach(client => {
+//     if (client.readyState === WebSocket.OPEN && client.username) {
+//       client.send(
+//         JSON.stringify({
+//           object: 'ping'
+//         })
+//       )
+//     }
+//   })
+// }, 3000)
 
 server.listen(process.env.PORT || 8080, () => {
-  console.log("Server started");
-});
+  console.log('Server started on port 8080')
+})
